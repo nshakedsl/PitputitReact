38929cb68a2e9993311ac5123ee7074f1d8c2086
OfirGurvits <ofirgurvits@gmail.com>
2023-05-10 13:41:26 +0300

    src/loginPage added background

--- src/components/Background-logo.js
+++ src/components/Background-logo.js
@@ -1,11 +1,11 @@
-import './Background-logo.css';
+import '../styles/login.css';
 //import logoColor from './logo-color.svg'
 
 
 function BackgroundLogo() {
     return (
       <div className="element_width center_content slide-in-left">
-          <img src="/public/logo-color.svg" className="logo" alt="default_user"/>
+          <img src="images/logo-color.svg" className="logo" alt="default_user"/>
       </div>
     );
   }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,21 +1,20 @@
 
 import './../styles/login.css'
+import BackgroundLogo from './../components/Background-logo';
 
 function LoginPage() {
   return (
         <div className="center_content background">
-          <img src="../pictures/background-left.png" alt="background-left" className="background-left" />
-          <img src="../pictures/background-right.png" alt="background-right" className="background-right" />
-          <div className="element_width slide-in-left">
-            <img src="../pictures/logo-color.svg" alt="logo" className="logo" />
-          </div>
+          <img src="images/background-left.png" alt="background-left" className="background-left" />
+          <img src="images/background-right.png" alt="background-right" className="background-right" />
+          <BackgroundLogo/>
           <form>
             <div className="element_width slide-in-right">
               <h3 className="title"> Welcome Back👋</h3>
               <input className="input regular" type="text" name="user" placeholder="Username" />
               <div className="containLoginInput">
                 <input className="input password" type="password" name="pass" placeholder="Password" />
-                <img className="eye" src="../pictures/eye-solid.svg" alt="eye" />
+                <img className="eye" src="images/eye-solid.svg" alt="eye" />
               </div>
     
               <button type="button" onClick={() => window.location.href='chats.html'} className="btn btn-info">Log in</button><br />
