eb1676fd3b333d31531dd2375ff3ae1efe38dd51
OfirGurvits <ofirgurvits@gmail.com>
2023-05-11 18:12:21 +0300

    added import react

--- src/chatComponents/UserDetails.js
+++ src/chatComponents/UserDetails.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './../styles/login.css'
 import './../styles/chats.css'
 function UserDetails({nameMe, myImg }) {
--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './../styles/login.css'
 
 function ImageUploader() {
