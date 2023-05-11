9d4b4ac763b88412a8e939e7cee322c536095f54
naamamat <naama.matzliach@gmail.com>
2023-05-11 13:47:55 +0300

    click on buttons

--- src/App.js
+++ src/App.js
@@ -4,27 +4,20 @@ import './App.css';
 import RegisterPage from './pages/RegisterPage';
 import ChatPage from './pages/ChatPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-  <LoginPage />
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/chats" element={<ChatPage />} />
+    </Routes>
+  </Router>
+
   );
 }
 
