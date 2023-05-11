0ab608259611b00b04e961282c06da01f85d04c6
OfirGurvits <ofirgurvits@gmail.com>
2023-05-11 15:37:14 +0300

    html placed in chats

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -77,15 +77,16 @@ function ChatPage() {
                             </div>
                             <div class="triangle triangle_other"></div>
                         </div>
+                        <footer class="footer">
+                            <input class="message" placeholder="Type a message" />
+                            <svg class="send" viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet"
+                                version="1.1" x="0px" y="0px" enable-background="new 0 0 24 24">
+                                <path fill="#54656f"
+                                    d="M1.101,21.757L23.8,12.028L1.101,2.3l0.011,7.912l13.623,1.816L1.112,13.845 L1.101,21.757z"></path>
+                            </svg>
+                        </footer>
                     </div>
-                    <footer class="footer">
-                        <input class="message" placeholder="Type a message" />
-                        <svg class="send" viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet"
-                            version="1.1" x="0px" y="0px" enable-background="new 0 0 24 24" xml:space="preserve">
-                            <path fill="#54656f"
-                                d="M1.101,21.757L23.8,12.028L1.101,2.3l0.011,7.912l13.623,1.816L1.112,13.845 L1.101,21.757z"></path>
-                        </svg>
-                    </footer>
+
 
                 </div>
 
