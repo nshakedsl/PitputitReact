25f97aa515801f307962773ff50b660a3b16b6f7
naamamat <naama.matzliach@gmail.com>
2023-05-13 18:02:49 +0300

    tooltip

--- src/components/enteranceComponents/PasswordInput.js
+++ src/components/enteranceComponents/PasswordInput.js
@@ -1,5 +1,8 @@
 
 import { useState } from 'react';
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 function PasswordInput({ placeholder, value, setValue }) {
   const [showPassword, setShowPassword] = useState(false);
@@ -8,27 +11,38 @@ function PasswordInput({ placeholder, value, setValue }) {
     setShowPassword(!showPassword);
   };
 
+
+  const renderTooltip = props => (
+    <Tooltip className="my-tooltip" {...props}> This is mandatory field. Must contain 8-32 charcters.</Tooltip>
+  );
+
+  const renderTooltipEye = props => (
+    <Tooltip className="my-tooltip" {...props}>{showPassword ? "Put my glasses on to hide my password🕶️ " : "Take off my glasses to see my password👁️ "} </Tooltip>
+  );
+
   return (
     <div className="containLoginInput">
-      {setValue && (
-        <input
-          data-toggle="tooltip" data-placement="left" data-html="true" title="This is mandatory field. Must contain 8-32 letters and numbers."
-          className="input regular"
-          type={showPassword ? 'text' : 'password'}
-          name="user"
-          placeholder={placeholder}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      )}
+      <OverlayTrigger placement="left" overlay={renderTooltip}>
+        {setValue && (
+          <input
+            className="input regular"
+            type={showPassword ? 'text' : 'password'}
+            name="user"
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        )}
+      </OverlayTrigger>
 
-      <img
-        className="eye"
-        src={showPassword ? "images/Picture6.png" : "images/Picture13.png"}
-        alt="eye"
-        onClick={handleTogglePassword} data-toggle="tooltip" data-placement="top" title="remove my glasses🕶️
-        to see my password"
-      />
+      <OverlayTrigger placement="top" overlay={renderTooltipEye}>
+        <img
+          className="eye"
+          src={showPassword ? "images/Picture6.png" : "images/Picture13.png"}
+          alt="eye"
+          onClick={handleTogglePassword} />
+
+      </OverlayTrigger>
     </div>
   );
 }
--- src/components/enteranceComponents/RegularInput.js
+++ src/components/enteranceComponents/RegularInput.js
@@ -1,10 +1,26 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
 
 function RegularInput({ placeholder, value, setValue }) {
+
+  const renderTooltip = props => (
+    <Tooltip className="my-tooltip" {...props}> This is mandatory field. Must contain 2-32 charcters.</Tooltip>
+  );
+
+
   return (
-    <div>
-      {setValue && (<input data-toggle="tooltip" data-placement="left" data-html="true" title="This is mandatory field. Must contain 2-32 charcters."
+    <div className="App">
+      <OverlayTrigger placement="left" overlay={renderTooltip}>
+      {setValue && (<input 
         data-bs-trigger="click" className="input regular" type="text" name="user" placeholder={placeholder} value={value}
         onChange={(e) => setValue(e.target.value)} />)}
+      </OverlayTrigger>
     </div>
   );
 }
