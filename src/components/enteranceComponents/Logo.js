f3845fe984d580186c0e90beb2cfa773120370cb
nshakedsl <shakedsolomon@gmail.com>
2023-05-14 11:43:55 +0300

    fix logo size

--- src/components/enteranceComponents/Logo.js
+++ src/components/enteranceComponents/Logo.js
@@ -1,12 +1,11 @@
 import React from "react";
-// import '../../styles/login.css';
-//import logoColor from './logo-color.svg'
+
 
 
 function Logo() {
   return (
     <div className="element_width center_content slide-in-left">
-      <img src="images/logo-color.svg" className="logo" alt="default_user" />
+      <img src="images/logo-color.svg" className="logo_big" alt="default_user" />
     </div>
   );
 }
