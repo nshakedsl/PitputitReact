291bf7e8dca080c284780600e9f40ea4d053f960
nshakedsl <shakedsolomon@gmail.com>
2023-05-15 17:04:13 +0300

    can't go to chts if not connected

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import React, { useContext } from 'react';
 import LoginPage from './pages/LoginPage';
 import './App.css';
 import RegisterPage from './pages/RegisterPage';
 import ChatPage from './pages/ChatPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { UserContext } from "./ctx/userContext"
 
 function App() {
+  const Userctx = useContext(UserContext);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage/>} />
-        <Route path="/register" element={<RegisterPage/>} />
-        <Route path="/chats" element={<ChatPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        {Userctx.userName && < Route path="/chats" element={<ChatPage />} />}
       </Routes>
     </Router>
 
