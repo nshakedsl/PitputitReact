e2677a635bc3ad85ffbecb9b7929f6a58b45352a
OfirGurvits <ofirgurvits@gmail.com>
2023-05-31 15:31:32 +0300

    bugfix in controllers

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,10 +3,10 @@ const getUsers = async (req, res) => {
     res.json(await userService.getUsers());
 };
 const getUser = async (req, res) => {
-    if(!req.params.username || req.params.username === ""){
+    if(!req.body.username || req.body.username === ""){
         return res.status(400).json({ errors: ['Bad Request of User'] });
     }
-    const user = await messageService.getUserByName(req.params.username);
+    const user = await messageService.getUserByName(req.body.username);
     if (!user) {
         return res.status(404).json({ errors: ['User not found'] });
     }
