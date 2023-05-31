502a0409cda63a68fe952025a8e4742a1a2d6026
naamamat <naama.matzliach@gmail.com>
2023-05-31 19:24:56 +0300

    add routes

--- server/app.js
+++ server/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const chats = require('./routes/chat');
+const chat = require('./routes/chat');
+const message = require('./routes/message');
+const user = require('./routes/user');
+const userPassName = require('./routes/userPassName');
 require('custom-env').env(process.env.NODE_ENV, './config');
 mongoose.connect(process.env.CONNECTION_STRING,
     {
@@ -13,5 +16,8 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/chats', chats);
+app.use('/chat', chat);
+app.use('/message', message);
+app.use('/user', user);
+app.use('/userPassName', userPassName);
 app.listen(process.env.PORT);
