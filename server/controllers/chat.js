8e0d4687d348f5eb004824dc3fb268f125952fc4
OfirGurvits <ofirgurvits@gmail.com>
2023-05-31 12:06:26 +0300

    added chat creation

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -9,5 +9,20 @@ const getChat = async (req, res) => {
     }
     res.json(chat);
 };
+const deleteChat = async (req, res) => {
+    const chat = await chatService.deleteChatById(req.params.id);
+    if (!chat) {
+        return res.status(404).json({ errors: ['Chat not found'] });
+    }
+    res.json(chat);
+};
+const createChat = async (req, res) => {
+    const me = "";
+    const chat = await chatService.createChat(req.params.sender,me);
+    if (!chat) {
+        return res.status(404).json({ errors: ['error when creating chat'] });
+    }
+    res.json(chat);
+};
 //...
-module.exports = { getChats,getChat };
+module.exports = { createChat,getChats,getChat,deleteChat };
