de7de09ba012b317f834ed59804074a6ca254c2a
naamamat <naama.matzliach@gmail.com>
2023-05-31 17:56:55 +0300

    validation in server

--- server/controllers/userPassName.js
+++ server/controllers/userPassName.js
@@ -1,17 +1,44 @@
 const userPassNameService = require('../services/userPassName');
 
 const registerUser = async (req, res) => {
-    /*
-    if(!req.body.username || req.body.username === ""){
-        return res.status(400).json({ errors: ['Bad Request of User'] });
-    }
-    const user = await messageService.getUserByName(req.body.username);
-    if (!user) {
-        return res.status(404).json({ errors: ['User not found'] });
-    }
-    */
+
+    if (!req.body.password.length < 8) {
+        return res.status(400).json({ errors: ['Password must contain at least 8 characters'] });
+
+    } else if (!/[0-9]/.test(!req.body.password) || !/[a-z]/.test(!req.body.password) || !/[A-Z]/.test(!req.body.password)) {
+        return res.status(400).json({ errors: ['Password must contain a combination of uppercase and lowercase letters and numbers'] });  
+
+    } else if (!/^[a-zA-Z0-9\._:\-\?!]+$/.test(req.body.username) || !/^[a-zA-Z0-9\._:\-\?!]+$/.test(req.body.displayName) || !/^[a-zA-Z0-9\._:\-\?!]+$/.test(req.body.password)) {
+        return res.status(400).json({ errors: ['You choose invalid characters'] });   
+
+    } else if (req.body.profilePic === 'images/user.png') {
+        return res.status(400).json({ errors: ['image is a mandatory field'] });   
+
+    } else if (req.body.username.length < 2 || req.body.displayName.length < 2){
+        return res.status(400).json({ errors: ['inputs must contain at least 2 characters'] }); 
+
+    } else if (req.body.username.length > 32 || req.body.displayName.length > 32 || req.body.password.length > 32){
+        return res.status(400).json({ errors: ['inputs must contain until 32 characters'] }); 
+
+    } 
+    
+    // try {
+    //     let res = await register(newUser);
+    //     if (res.ok) {
+    //         //Todo: Ofir do if it's ok - good!
+    //     }
+
+    // } catch (err) {
+    //    return res.status(404).json({ errors: ['err: ', err] });
+
+    // }
+
     res.json(user);
 
 };
 //...
 module.exports = { registerUser };
+
+
+
+
