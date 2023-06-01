680e0d4ac39c5f4e73f53a0808e56b78f0f101b4
naamamat <naama.matzliach@gmail.com>
2023-06-01 16:27:35 +0300

    update

--- server/controllers/userPassName.js
+++ server/controllers/userPassName.js
@@ -1,6 +1,10 @@
 const userPassNameService = require('../services/userPassName');
 
 const registerUser = async (req, res) => {
+    if(!req || !req.body || !req.body.password || !req.body.username || !req.body.displayName){
+        return res.status(401).json({ errors: ['Mising '] });
+        //TODO:everyoneeeee
+    }
 
 
     if (req.body.password.length < 8) {
