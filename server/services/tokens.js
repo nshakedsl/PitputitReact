b49a82fffd8cf35152db0a228b90887cb88bda9f
nshakedsl <shakedsolomon@gmail.com>
2023-06-01 17:14:48 +0300

    on working

--- server/services/tokens.js
+++ server/services/tokens.js
@@ -3,6 +3,17 @@ const User = require('../models/user');
 
 const login = async (username, password) => {
     const user = await User.findOne({ username, password });
+    if (user) {
+        const data = { username }
+        // Generate the token.
+        const token = jwt.sign(data, key)
+        // Return the token to the browser
+        res.status(201).json({ token });
+
+    }
+    return null
+
+
     console.log('user: ', user);
 };
 
