0c7b84bcce48043fab1314d3a915b537a03e6e58
nshakedsl <shakedsolomon@gmail.com>
2023-06-01 21:05:13 +0300

    login workinggg

--- server/controllers/tokens.js
+++ server/controllers/tokens.js
@@ -4,10 +4,11 @@ const login = async (req, res) => {
     if (!req.body.username || req.body.username === "" || !req.body.password || req.body.password === "") {
         return res.status(400).json({ errors: ['All fields must be initialized'] });
     }
-    const ans = tokenService.login(req.body.username, req.body.password);
+    const ans = await tokenService.login(req.body.username, req.body.password);
     if (!ans) {
         return res.status(404).json({ errors: ['username or password incorrect'] });
     }
-    return res.status(200).json(ans);
+    console.log('ans: ', ans);
+    return res.send(ans);
 };
 module.exports = { login };
--- server/services/tokens.js
+++ server/services/tokens.js
@@ -1,25 +1,22 @@
 const UserPassName = require('../models/userPassName');
 const userService = require('./user');
+const jwt = require("jsonwebtoken")
 
 const login = async (username, password) => {
     try {
         const userObj = await userService.getUserByName(username);
-        if(!userObj){
+        if (!userObj) {
             return null;
-        }
-        console.log('password: ', password);
-        console.log('username: ', username);
+        };
         const user = await UserPassName.findOne({
             user: userObj,
             password: password
         });
 
-        console.log('user: ', user);
         if (user) {
             const data = { username }
             // Generate the token.
             const token = jwt.sign(data, process.env.SECRET_KEY, { expiresIn: '1h' })
-            console.log('token: ', token);
             // Return the token to the browser
             return token
         }
