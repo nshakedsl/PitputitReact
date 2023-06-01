74ff5795fa11ab4f283081b07a36e6a6b8d7554b
nshakedsl <shakedsolomon@gmail.com>
2023-06-01 16:47:53 +0300

    add rout to token

---
+++ server/routes/tokens.js
@@ -0,0 +1,10 @@
+const tokenController = require('../controllers/token');
+
+const express = require('express');
+var router = express.Router();
+
+router.route('/')
+    .post(tokenController.login);
+
+
+module.exports = router;
