87fb3b71e6245019bed67d198560b4925e3abe36
nshakedsl <shakedsolomon@gmail.com>
2023-06-02 12:02:43 +0300

    change messageservice to userservice

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,7 +4,7 @@ const getUser = async (req, res) => {
     if (!req.body.username || req.body.username === "") {
         return res.status(400).json({ errors: ['Bad Request of User'] });
     }
-    const user = await messageService.getUserByName(req.body.username);
+    const user = await userService.getUserByName(req.body.username);
     if (!user) {
         return res.status(404).json({ errors: ['User not found'] });
     }
